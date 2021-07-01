a2e49bb7d2fbb90abb18ac5992950325ff52b139
Ji Yan <ji.yan@hcl.com>
2021-07-01 23:35:10 +0800

    enable push

--- volt-mx-tutorials/_next/static/chunks/webpack.js
+++ volt-mx-tutorials/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "487493c803483bd389e4";
+/******/ 	var hotCurrentHash = "0047b1daf2923294f742";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

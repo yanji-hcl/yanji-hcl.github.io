ee444a409efae8e91e79567899ab9de2aab20990
Ji Yan <ji.yan@hcl.com>
2021-07-01 13:22:51 +0800

    use new exported files

---
+++ volt-mx-tutorials/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/hikes":["static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js","static\u002Fchunks\u002Fpages\u002Fhikes.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhikes"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

16ad0ff476dc8de6a8969465ec9c96a5c92b7332
Ji Yan <ji.yan@hcl.com>
2021-07-01 18:35:23 +0800

    add log in switch lang

--- volt-mx-tutorials/_next/static/chunks/webpack.js
+++ volt-mx-tutorials/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7c826ddbd69f0cc6cbf0";
+/******/ 	var hotCurrentHash = "3584bca5bb95e005ae44";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

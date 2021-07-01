83fbde5518d5dc7cbdfc4acef3e79ea97fe8cbce
Ji Yan <ji.yan@hcl.com>
2021-07-01 23:48:54 +0800

    another approach

--- volt-mx-tutorials/_next/static/chunks/webpack.js
+++ volt-mx-tutorials/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0047b1daf2923294f742";
+/******/ 	var hotCurrentHash = "f1fd8a8e769fabd48e5b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

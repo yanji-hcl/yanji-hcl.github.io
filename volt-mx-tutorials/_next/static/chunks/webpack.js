abf16fdf8fbaf5ba2c430acc48da7cce1f4ba99b
Ji Yan <ji.yan@hcl.com>
2021-07-04 11:49:16 +0800

    reload

--- volt-mx-tutorials/_next/static/chunks/webpack.js
+++ volt-mx-tutorials/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f6d1dacee2c3bdb37b16";
+/******/ 	var hotCurrentHash = "599c38edf26b9b97f3d9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
